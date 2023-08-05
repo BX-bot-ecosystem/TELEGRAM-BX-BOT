a006d10faa3e76e156ae9bd3025ec0b94c152dcf
GianlucaOberreit <gianluca.oberreit@polytechnique.edu>
2023-08-05 15:59:36 +0200

    Create basic example

---
+++ src/Committees/example.py
@@ -0,0 +1,21 @@
+import time
+import re
+from telegram import ReplyKeyboardMarkup, Update
+from telegram.ext import (
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
+from utils import config, db
+
+import base
+
+
+class Example(base.Committee):
+    def __init__(self):
+        super().__init__(
+            'example',
+        )
+    
