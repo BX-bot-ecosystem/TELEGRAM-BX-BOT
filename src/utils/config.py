423fef513cd3c3e8b489e076dbe0812e29d678b5
GianlucaOberreit <gianluca.oberreit@polytechnique.edu>
2023-08-05 15:58:07 +0200

    Add committees.json reading

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 import redis
+import json
 
 ROOT = str(Path(__file__).parent.parent.parent)
 
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+with open(ROOT + "/data/Committees/committees.json") as f:
+    committees_info = json.load(f)
