f99aebeb906c36ccb53217386c7a3fce5c458114
GianlucaOberreit <gianluca.oberreit@polytechnique.edu>
2023-08-11 23:54:17 +0200

    move redis host and port values to env

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,10 +1,16 @@
 from pathlib import Path
 import redis
 import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+_REDIS_HOST = os.getenv("REDIS_HOST")
+_REDIS_PORT = int(os.getenv("REDIS_PORT"))
 
 ROOT = str(Path(__file__).parent.parent)
 
-r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+r = redis.Redis(host=_REDIS_HOST, port=_REDIS_PORT, decode_responses=True)
 
 with open(ROOT + "/data/Committees/committees.json") as f:
     committees_info = json.load(f)
