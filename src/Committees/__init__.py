44873706d3cb4e723b4dc109648ff7146a64cb63
GianlucaOberreit <gianluca.oberreit@polytechnique.edu>
2023-08-13 17:18:29 +0200

    add Xcinema

--- src/Committees/__init__.py
+++ src/Committees/__init__.py
@@ -3,6 +3,7 @@ from .bar import Bar
 from .physiX import Physix
 from .example import Example
 from .rowing import Rowing
+from .xcinema import Xcinema
 
-_temp = [Bar, Physix, Example, Rowing]
+_temp = [Bar, Physix, Example, Rowing, Xcinema]
 committees = [ob().handler for ob in _temp]
