575bcc3fa3ab14ac21d49bafc40f385505fca426
GianlucaOberreit <gianluca.oberreit@polytechnique.edu>
2023-09-13 21:35:56 +0200

    Add PhysiX to json

--- src/Committees/__init__.py
+++ src/Committees/__init__.py
@@ -6,5 +6,6 @@ from .rowing import Rowing
 from .xcinema import Xcinema
 
 _temp = [Bar, Physix, Example, Rowing, Xcinema]
-names = [ob().name for ob in _temp]
-committees = [ob().handler for ob in _temp]
+_objs = [ob() for ob in _temp]
+names = [ob.name for ob in _objs]
+committees = [ob.handler for ob in _objs]
